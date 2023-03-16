e65d7124e8c966eb96646465619b48b210e93c36
belaymit <2belamit@gmail.com>
2023-03-16 19:34:54 +0300

    Added a test_case for reverse_string method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -17,4 +17,12 @@ RSpec.describe Solver do
       expect(@solver.factorial(-5)).to eq 'Please insert positive number'
     end
   end
+  context 'Test case for Reverse_string method' do
+    it 'Should return "olleh" when the given argument is "hello"' do
+      expect(@solver.reverse('hello')).to eq 'olleh'
+    end
+    it 'Should return "Please enter a string" when the given argument is not a string' do
+      expect(@solver.reverse(1)).to eq 'Please enter a string'
+    end
+  end
 end
